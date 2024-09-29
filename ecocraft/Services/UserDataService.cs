1349a9cb37441148ccae8b762a19a1ec8a0ec084
romain.pichard <romain.pichard@sasp.fr>
2024-09-29 12:28:14 +0200

    boucle infini sur sélection / déselection de skill

--- ecocraft/Services/UserDataService.cs
+++ ecocraft/Services/UserDataService.cs
@@ -1,4 +1,5 @@
 ﻿using ecocraft.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ecocraft.Services;
 
@@ -29,6 +30,46 @@ public class UserDataService(UserSkillDbService userSkillDbService,
         UserSettings = userSettingsTask.Result;
         UserElements = userElementsTask.Result;
         UserPrices = userPricesTask.Result;
+    }
+
+    public void AddUserSkill(UserSkill userSkill)
+    {
+		UserSkills.Add(userSkill);
+
+	}
+
+    public void RemoveUserSkill(UserSkill userSkill)
+    {
+        UserSkills.Remove(userSkill);
+    }
+
+    // Méthode pour mettre à jour les compétences de l'utilisateur
+    public List<UserSkill> UpdateUserSkills(UserServer userServer, List<Skill> selectedSkills)
+    {
+        // Récupérer les compétences actuelles de l'utilisateur
+        var existingUserSkills = UserSkills.Where(us => us.UserServerId == userServer.Id);
+
+        // Supprimer les compétences non sélectionnées
+        var skillsToRemove = existingUserSkills.Where(us => !selectedSkills.Any(s => s.Id == us.SkillId)).ToList();
+        foreach(var userSkill in skillsToRemove)
+            UserSkills.Remove(userSkill);            
+
+        // Ajouter les nouvelles compétences sélectionnées
+        foreach (var skill in selectedSkills)
+        {
+            if (!existingUserSkills.Any(us => us.SkillId == skill.Id))
+            {
+                var newUserSkill = new UserSkill
+                {
+                    UserServer = userServer,
+                    Skill = skill,
+                    Level = 0 // Ajuster le niveau si nécessaire
+                };
+                UserSkills.Add(newUserSkill);
+            }
+        }
+        return UserSkills;
+        //await _context.SaveChangesAsync();
     }
 
 }
