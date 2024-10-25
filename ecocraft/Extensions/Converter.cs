39df698ac12e0ce0e3389ece2b6babcbc0b01a43
Thibault Brocheton <brochetonthibault@gmail.com>
2024-10-25 05:14:22 +0200

    feat: lot of visual additions

---
+++ ecocraft/Extensions/Converter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using MudBlazor;
+
+namespace ecocraft.Extensions;
+
+public static class CultureInvariantConverter
+{
+    public static Converter<float> DotOrComma = new Converter<float>
+    {
+        SetFunc = value => $"{value}",
+        GetFunc = number =>
+        {
+            if (String.IsNullOrWhiteSpace(number)) return 0;
+            number = number.Replace(',', '.');
+            if (float.TryParse(number, NumberStyles.Any, CultureInfo.InvariantCulture, out float result))
+            {
+                return result;
+            }
+            return 0;
+        },
+    };
+}
