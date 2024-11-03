261d2fc4c48a7cdd50c69fe5bdf24ca734f88265
romain.pichard <romain.pichard@sasp.fr>
2024-11-03 23:37:59 +0100

    Change culture management and fix override price

--- ecocraft/Extensions/Converter.cs
+++ ecocraft/Extensions/Converter.cs
@@ -5,7 +5,22 @@ namespace ecocraft.Extensions;
 
 public static class CultureInvariantConverter
 {
-    public static Converter<float> DotOrComma = new Converter<float>
+    public static Converter<float> DotOrCommaFloat = new Converter<float>
+    {
+        SetFunc = value => $"{value}",
+        GetFunc = number =>
+        {
+            if (String.IsNullOrWhiteSpace(number)) return 0;
+            number = number.Replace(',', '.');
+            if (float.TryParse(number, NumberStyles.Any, CultureInfo.InvariantCulture, out float result))
+            {
+                return result;
+            }
+            return 0;
+        },
+    };
+
+    public static Converter<float?> DotOrCommaFloatNull = new Converter<float?>
     {
         SetFunc = value => $"{value}",
         GetFunc = number =>
