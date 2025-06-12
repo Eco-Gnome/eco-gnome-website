9722c4fb4fa5f1a873f87711c993dea88b0335b6
Thibault-Brocheton <brochetonthibault@gmail.com>
2025-06-12 15:53:44 +0200

    chore: add missing officiel icons

--- ecocraft/Scripts/export_icons.js
+++ ecocraft/Scripts/export_icons.js
@@ -7,7 +7,7 @@ const { Worker, isMainThread, parentPort, workerData } = require('worker_threads
 
 if (isMainThread) {
     // Variables en dur au début du fichier
-    const assetRipperPath = 'C:\\Users\\thiba\\Documents\\Repositories\\eco-calculator-website\\ecocraft\\External\\development\\AssetRipper.GUI.Free.exe'; // Chemin vers AssetRipper
+    const assetRipperPath = 'C:\\Users\\thiba\\Documents\\ECO\\AssetRipper_win_x64\\AssetRipper.GUI.Free.exe'; // Chemin vers AssetRipper
     const unityBundlePath = 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Eco\\Eco_Data\\StreamingAssets\\aa\\StandaloneWindows64\\icons_assets_all_f2c1768531085329e05269c0cce60db4.bundle'; // Chemin vers le bundle Unity
     const exportDirectory = 'C:\\Users\\thiba\\Documents\\Repositories\\eco-calculator-website\\ecocraft\\tmp'; // Chemin vers le dossier d'exportation
     const outputDirectory = 'C:\\Users\\thiba\\Documents\\Repositories\\eco-calculator-website\\ecocraft\\Exports\\OfficialIcons'; // Chemin vers le dossier de sortie final
